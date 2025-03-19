16c48123d1c6c9bfe534a6fc986d5d4a89f8b317
learn-offshore <pankaj.kr26@outlook.com>
2025-03-19 23:49:32 +0530

    Explain Basic Variables

---
+++ variables.py
@@ -0,0 +1,50 @@
+# Accepted characters for defining a variable
+
+a1 = True
+
+print(a)
+
+## Data Types
+False = ''
+#1. number
+r = 198
+print(type(r))
+
+#2. float
+pi = 3.142857
+print(type(pi))
+#3. boolean
+is_true = False
+
+
+
+#4. strings (single, double, triple)
+name = 'Hasseb'
+class12 = """It's a for class monday.
+This is a new line."""
+
+triple_string = '''Pankaj'''
+
+#5. List
+feesPerHour = 30
+pi = 3.142857
+r = 5
+
+areaOfCircle1 = pi * (r ** 2)
+areaOfCircle2 = pi * (5 ** 2)
+areaOfCircle3 = pi * (4 ** 2)
+
+# print(feesPerHour * 20)
+# print(feesPerHour + 20)
+# print(feesPerHour - 20)
+# print('Fees for Python class is ', feesPerHour)
+
+print(areaOfCircle1)
+print(areaOfCircle2)
+print(areaOfCircle3)
+
+
+# Assign same value to different variables
+
+
+# Type Casting
